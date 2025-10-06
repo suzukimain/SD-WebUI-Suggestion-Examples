e0323a254c99e42f7d46f95fa443bab217c64c34
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-10-06 11:09:13 +0900

    Update test_run.py

--- scripts/test_run.py
+++ scripts/test_run.py
@@ -10,14 +10,8 @@ def on_ui_tabs():
     with gr.Blocks() as demo:
         with gr.Tab("autocomplete"):
             gr.Markdown("## 🍎 Fruit Autocomplete Sample")
-
             textbox = gr.Textbox(label="Type a fruit name", elem_id="fruit-input")
-            output = gr.Textbox(label="Selected fruit")
-
-            # JS 側から選択結果を受け取るためのダミー
-            btn = gr.Button("Submit")
-            btn.click(fn=lambda x: x, inputs=textbox, outputs=output)
-
+                  
     return [(demo, "Autocomplete", "autocomplete_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
