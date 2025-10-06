95240e6c6e5e3c5710f2de36f5a2efa3420a0fb2
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-10-06 11:18:05 +0900

    Update test_run.py

--- scripts/test_run.py
+++ scripts/test_run.py
@@ -1,17 +1,13 @@
 import gradio as gr
 from modules import script_callbacks
 
-# 補完候補の果物リスト
-FRUITS = ["apple", "apricot", "banana", "blueberry", "cherry",
-          "grape", "kiwi", "lemon", "mango", "melon",
-          "orange", "peach", "pear", "pineapple", "strawberry", "watermelon"]
-
 def on_ui_tabs():
     with gr.Blocks() as demo:
         with gr.Tab("autocomplete"):
             gr.Markdown("## 🍎 Fruit Autocomplete Sample")
-            textbox = gr.Textbox(label="Type a fruit name", elem_id="fruit-input")
-                  
+            # elem_id を指定して JS 側から参照できるようにする
+            gr.Textbox(label="Type a fruit name", elem_id="fruit-input")
+
     return [(demo, "Autocomplete", "autocomplete_tab")]
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
